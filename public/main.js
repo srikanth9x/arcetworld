6d4bd824a6521db83266b3de4e8f8db764778744
Srikanth <srikanth9x@gmail.com>
2025-12-26 02:29:25 +0530

    Added social profiles

--- public/main.js
+++ public/main.js
@@ -3,6 +3,12 @@ const spell = document.getElementById("spell");
 const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
 const screenTxt = document.getElementById("screen");
+const discord = document.getElementById("discord");
+const instagram = document.getElementById("instagram");
+const x = document.getElementById("x");
+const youtube = document.getElementById("youtube");
+const github = document.getElementById("github");
+const mail = document.getElementById("mail");
 
 title.addEventListener("click", () => {
   spell.play();
@@ -17,4 +23,28 @@ yesBtn.addEventListener("click",() => {
   setTimeout(() => {
     window.location.href = "https://forms.gle/QDHCkXPj2ZTrscSw6"
   }, 1200);
+});
+
+discord.addEventListener("click", () => {
+  window.location.href = "https://discord.com/users/1453759433716011039/"
+});
+
+instagram.addEventListener("click", () => {
+  window.location.href = "https://instagram.com/arcetworld/"
+});
+
+x.addEventListener("click", () => {
+  window.location.href = "https://x.com/arcetworld/"
+});
+
+youtube.addEventListener("click", () => {
+  window.location.href = "https://youtube.com/@arcetworld/"
+});
+
+github.addEventListener("click", () => {
+  window.location.href = "https://github.com/arcetworld/"
+});
+
+mail.addEventListener("click", () => {
+  window.location.href = "mailto:arcet.web.app@gmail.com"
 });
